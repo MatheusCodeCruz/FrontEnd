e718969f2f664753a601532a2e95eae1f1433b45
199417 <matheus.dacruz762@gmail.com>
2023-04-29 12:53:57 -0300

    aulas29/04

---
+++ AULA03/index.js
@@ -0,0 +1,18 @@
+console.log("Olá, mundo!");
+
+let nome = "Matheus";
+const pi = 3.14;
+let isrunning = false;
+const pessoa = {
+    nome: "Matheus",
+    idade: 28,
+    profissao: "aluno"
+}
+
+console.log(typeof nome);
+console.log(pi);
+console.log(isrunning);
+console.log(typeof pessoa);
+
+let frase = "Meu nome é " +pessoa.nome+". Eu tenho " +pessoa.idade+ "anos."
+console.log(frase);
